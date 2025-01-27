802539234bc8a8a5ed2e3b14eb28efa0e05d8196
masaki.nakano <bdyht515@icloud.com>
2025-01-27 10:55:13 +0900

    コンポーネントからAPIを叩くよう実装

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants = () => {
+  const hasFetched = useRef(false);
+
+  useEffect(() => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       レストラン一覧
