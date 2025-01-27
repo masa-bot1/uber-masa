1e83235b2bf5549f8cd16d620d7934e3e859cb93
masaki.nakano <bdyht515@icloud.com>
2025-01-27 17:36:01 +0900

    フード一覧のAPIを叩く

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,6 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchFoods } from '../apis/foods';
 
 export const Foods = ({ restaurantsId }) => {
+  useEffect(() => {
+    fetchFoods(restaurantsId)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       フード一覧
